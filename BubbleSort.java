be5e19411b0651c69fa3569f0409328f34c9deda
Brijesh Patel <Brijeshrpatel9@users.noreply.github.com>
2014-11-19 07:00:46 -0600

    Create BubbleSort.java

---
+++ BubbleSort.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+// Ascending order
+
+public class BubbleSort
+{
+	public static void bubble_sort(int[] data)
+	{
+		int temp = 0;
+		int n = data.length;
+		for(int i = 0; i < (n-1); i++)
+		{
+			for(int j = 1; j < (n-i); j++)
+			{
+				if(data[j-1] > data[j])
+				{
+					temp = data[j-1];
+					data[j-1] = data[j];
+					data[j] = temp;
+				}
+			}
+			System.out.print(Arrays.toString(data)+ ", ");
+			System.out.println("");
+		}	
+	}
+
+	public static void main(String argv[])
+	{
+		int[] data = {2,45, 23, 56, 34, 886, 5,99, 548, 10};
+		bubble_sort(data);
+	}
+}
