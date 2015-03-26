9a2a106a0d9b24336e850a4de3a5869710fae89a
Brijesh Patel <Brijeshrpatel9@users.noreply.github.com>
2015-03-26 06:27:07 -0500

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -14,24 +14,23 @@ public class InsertionSort
 	
 		for (int i = 1; i < array.length; i++)		// loop through the unsorted array
 		{
-							// (the first element is considered sorted)
+								// (the first element is considered sorted)
 			temp = array[i];			// grab the first element of the unsorted array
 			pos = i - 1;				// get the index of the last sorted element
 		
 			while ((pos >= 0) && (temp < array [pos]))	// while the current sorted element is greater than temp
 			{
-			array[pos + 1] = array[pos];		// keep shifting sorted elements back by 1
-			pos--;					// decrement the pos index
+				array[pos + 1] = array[pos];		// keep shifting sorted elements back by 1
+				pos--;					// decrement the pos index
+			}
+			array[pos + 1] = temp;				// insert temp such that array[pos] <= temp < array[pos + 2]
+			System.out.print(Arrays.toString(array) + ", ");
+			System.out.println("");
 		}
-		array[pos + 1] = temp;				// insert temp such that array[pos] <= temp < array[pos + 2]
-		System.out.print(Arrays.toString(array) + ", ");
-		System.out.println("");
 	}
-}
 	public static void main(String args[])
 	{
 		int[] data = {2, 45, 23, 56, 34, 886, 5, 99, 548, 10};
 		insertionSort(data);
 	}
 }
- 
