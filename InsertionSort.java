fab8501d32b0a4a2996eb2be327dd3459bba09ba
Brijesh Patel <Brijeshrpatel9@users.noreply.github.com>
2015-03-26 06:26:23 -0500

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -2,23 +2,24 @@ import java.util.Arrays;
 
 public class InsertionSort
 {
+	//pre: array is full, all elements are non-null integers
+	//post: the array is sorted in ascending order
+
 	public static void insertionSort(int array[])
-//pre: array is full, all elements are non-null integers
-//post: the array is sorted in ascending order
-{
-	int temp;		// this variable is the element of the unsorted array currently
-				// being compared to elements of the sorted array
-	int pos;		// this variable keeps track of where in the sorted array the 
-				// comparison takes place
-	
-	for (int i = 1; i < array.length; i++)		// loop through the unsorted array
 	{
-							// (the first element is considered sorted)
-		temp = array[i];			// grab the first element of the unsorted array
-		pos = i - 1;				// get the index of the last sorted element
-		
-		while ((pos >= 0) && (temp < array [pos]))	// while the current sorted element is greater than temp
+		int temp;		// this variable is the element of the unsorted array currently
+					// being compared to elements of the sorted array
+		int pos;		// this variable keeps track of where in the sorted array the 
+					// comparison takes place
+	
+		for (int i = 1; i < array.length; i++)		// loop through the unsorted array
 		{
+							// (the first element is considered sorted)
+			temp = array[i];			// grab the first element of the unsorted array
+			pos = i - 1;				// get the index of the last sorted element
+		
+			while ((pos >= 0) && (temp < array [pos]))	// while the current sorted element is greater than temp
+			{
 			array[pos + 1] = array[pos];		// keep shifting sorted elements back by 1
 			pos--;					// decrement the pos index
 		}
